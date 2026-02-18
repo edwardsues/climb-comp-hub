bbd9e598cd8f6fa939f5bced1345c858660d9b3b
Edward Su <edward.su29@gmail.com>
2026-02-18 16:41:26 -0500

    add auth decorator and get/create user function

--- backend/models.py
+++ backend/models.py
@@ -16,8 +16,8 @@ class User(db.Model):
     __tablename__ = 'users'
 
     id = db.Column(db.Integer, primary_key=True)
+    auth0_id = db.Column(db.String(255), unique=True, nullable=False)
     email = db.Column(db.String(255), unique=True, nullable=False)
-    password_hash = db.Column(db.String(255), nullable=False)
     name = db.Column(db.String(255), nullable=False)
     dob = db.Column(db.Date)
     role = db.Column(db.String(20), default='climber')
@@ -44,6 +44,7 @@ class Registration(db.Model):
     competition_id = db.Column(db.Integer, db.ForeignKey('competitions.id'), primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), primary_key=True)
     registered_at = db.Column(db.DateTime, default=datetime.now(timezone.utc))
+    
 class Climb(db.Model):
     __tablename__ = 'climbs'
     
