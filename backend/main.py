34dcf3864461011d9fe92bdeac46537539850468
Edward Su <edward.su29@gmail.com>
2026-03-04 17:43:43 -0500

    create GET registrations and /register

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,8 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
 from config import Config
-from models import db, Gym, Competition, Climb
+from models import db, Gym, Competition, Climb, Registration
+from auth import require_auth, get_or_create_user
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -34,7 +35,8 @@ def get_comps():
         'id': comp.id,
         'gym_id': comp.gym_id,
         'name': comp.name,
-        'date': comp.date.isoformat(),
+        'start_time': comp.start_time.isoformat(),
+        'end_time': comp.end_time.isoformat(),
         'status': comp.status
     } for comp in comps]), 200
 
@@ -50,7 +52,8 @@ def get_comp_details(comp_id):
         'id': comp_id,
         'gym_id': comp.gym_id,
         'name': comp.name,
-        'date': comp.date.isoformat(),
+        'start_time': comp.start_time.isoformat(),
+        'end_time': comp.end_time.isoformat(),
         'status': comp.status,
         'climbs': [{
             'id': climb.id,
@@ -60,6 +63,57 @@ def get_comp_details(comp_id):
             'image_url': climb.image_url
         } for climb in climbs]
         }), 200
+
+#####################################
+#           climber routes          #
+#####################################
+@app.route("/api/registrations", methods=["GET"])
+@require_auth
+def view_registered_comps():
+    """View which competitions the current user is registered for."""
+    user = get_or_create_user()
+
+    registrations = Registration.query.filter_by(user_id=user.id).all()
+    return jsonify([{
+        'id': reg.competition.id,
+        'name': reg.competition.name,
+        'start_time': reg.competition.start_time.isoformat(),
+        'end_time': reg.competition.end_time.isoformat(),
+        'status': reg.competition.status
+    } for reg in registrations]), 200
+
+
+
+@app.route("/api/competitions/<int:comp_id>/register", methods=["POST"])
+@require_auth
+def register_for_comp(comp_id):
+    comp = Competition.query.get_or_404(comp_id)
+
+    # get the user
+    user = get_or_create_user()
+
+    # check if the user is registered for this competition
+    this_reg = Registration.query.filter_by(
+        user_id = user.id, competition_id=comp.id
+    ).first()
+    if this_reg:
+        return jsonify({"error": "Already registered for this competition"}), 409
+    
+    # check if the user is registered for another competition at this time
+    overlapping = Registration.query.join(Competition).filter(
+        Registration.user_id == user.id,
+        Competition.start_time < comp.end_time,
+        Competition.end_time > comp.start_time
+    ).first()
+    if overlapping:
+        return jsonify({"error": "already registered for a competition during this time"}), 400
+    
+    new_reg = Registration(user_id = user.id, competition_id = comp.id)
+    db.session.add(new_reg)
+    db.session.commit()
+
+    return jsonify({"message": "Successfully registered"}), 201
+    
 
 # Run the app
 if __name__ == '__main__':
